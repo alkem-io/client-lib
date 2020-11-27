41e26b964180a52e1eacb169dbab20789166568f
Angel <atstojanov@gmail.com>
2020-11-27 19:03:12 +0200

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import { CherrytwistClient } from './CherrytwistClient';
-// import { EnvironmentFactory } from './util/EnvironmentFactory';
-
 export * from './CherrytwistClient';
-// export * from './util/EnvironmentFactory';
