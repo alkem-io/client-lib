2c07da5e34a7566cb6d47eff0e8eccfde51dcbe9
Neil Smyth <neil@thesmyths.eu>
2021-06-21 10:11:09 +0200

    updated scalar types

--- src/types/cherrytwist-schema.ts
+++ src/types/cherrytwist-schema.ts
@@ -14,19 +14,19 @@ export type Scalars = {
   Int: number;
   Float: number;
   /** A decentralized identifier (DID) as per the W3C standard. */
-  DID: any;
+  DID: string;
   /** A date-time string at UTC, such as 2019-12-03T09:54:33Z, compliant with the date-time format. */
-  DateTime: any;
+  DateTime: Date;
   /** The `JSON` scalar type represents JSON values as specified by [ECMA-404](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf). */
   JSON: any;
   /** A human readable identifier, 3 <= length <= 25. Used for URL paths in clients. Characters allowed: a-z,A-Z,0-9. */
-  NameID: any;
+  NameID: string;
   /** A uuid identifier. Length 36 charachters. */
-  UUID: any;
+  UUID: string;
   /** A UUID or NameID identifier. */
-  UUID_NAMEID: any;
+  UUID_NAMEID: string;
   /** A UUID or Email identifier. */
-  UUID_NAMEID_EMAIL: any;
+  UUID_NAMEID_EMAIL: string;
   /** The `Upload` scalar type represents a file upload. */
   Upload: File;
 };
