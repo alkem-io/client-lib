2e6444da47c15176e851197b5cf01006fb7a61d8
svetoslav <svetoslav@alkem.io>
2023-05-16 09:18:06 +0300

    removed redundant code

--- src/util/log.error.ts
+++ src/util/log.error.ts
@@ -2,7 +2,7 @@ import { AlkemioClientError } from '../types';
 
 export const logError = (error: AlkemioClientError) => {
   if (error.response?.errors) {
-    error.response.errors.map(err => console.error(err.message));
+    error.response.errors.forEach(err => console.error(err.message));
   } else if (error.message) {
     console.error(error.message);
   }
